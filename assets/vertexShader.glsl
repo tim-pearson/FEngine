72b58bea1a2763533382854886668bacada8d7d5
florian.laine <florian.laine@epita.fr>
2023-12-01 02:19:31 +0000

    Add transofrm and camera velocity

--- assets/vertexShader.glsl
+++ assets/vertexShader.glsl
@@ -1,7 +1,11 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
 
+uniform mat4 transform;
+uniform mat4 view;
+uniform mat4 projection;
+
 void main()
 {
-    gl_Position = vec4(aPos.xyz, 1.0);
+    gl_Position = projection * view * transform * vec4(aPos, 1.0);
 }
